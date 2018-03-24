fc211a14ca09fbe420645718e3e4d1342d76f4f4
Joe Gaebel <joe.gaebel@hotmail.com>
2018-03-24 14:41:16 +1100

    Try preloading the video natively

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -4,21 +4,23 @@ video.addEventListener('click', () => {
     video.play();
 });
 
-let req = new XMLHttpRequest();
-req.open('GET', '/video', true);
-req.responseType = 'blob';
+video.play();
 
-req.onload = function() {
-    if (this.status === 200) {
-        let videoBlob = this.response;
-        let vid = URL.createObjectURL(videoBlob);
-        video.src = vid;
-        video.play();
-    }
-};
-
-req.onerror = function() {
-    console.log("wah wah");
-};
-
-req.send();
+// let req = new XMLHttpRequest();
+// req.open('GET', '/video', true);
+// req.responseType = 'blob';
+//
+// req.onload = function() {
+//     if (this.status === 200) {
+//         let videoBlob = this.response;
+//         let vid = URL.createObjectURL(videoBlob);
+//         video.src = vid;
+//         video.play();
+//     }
+// };
+//
+// req.onerror = function() {
+//     console.log("wah wah");
+// };
+//
+// req.send();
