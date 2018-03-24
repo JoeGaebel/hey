797606de738a39d26f2195bb352a5204de5d5591
Joe Gaebel <joe.gaebel@hotmail.com>
2018-03-24 15:37:20 +1100

    Try loading with js to override chrome data saver

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,7 +1,21 @@
-const video = document.getElementById('video');
+let req = new XMLHttpRequest();
+req.open('GET', '/video', true);
+req.responseType = 'blob';
 
-video.addEventListener('click', () => {
-    video.play();
-});
+req.onload = function () {
+    if (this.status === 200) {
+        const videoBlob = this.response;
+        const vid = URL.createObjectURL(videoBlob);
+        const video = document.getElementById('video');
 
-video.play();
+        video.addEventListener('click', () => { video.play(); });
+        video.src = vid;
+        video.play();
+    }
+};
+
+req.onerror = function () {
+    console.log("wah wah");
+};
+
+req.send();
