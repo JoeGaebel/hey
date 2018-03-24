84ee5a3576f2296bb13040024b37c33d2095acf9
Joe Gaebel <joe.gaebel@hotmail.com>
2018-03-24 14:53:57 +1100

    Remove old js

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -5,22 +5,3 @@ video.addEventListener('click', () => {
 });
 
 video.play();
-
-// let req = new XMLHttpRequest();
-// req.open('GET', '/video', true);
-// req.responseType = 'blob';
-//
-// req.onload = function() {
-//     if (this.status === 200) {
-//         let videoBlob = this.response;
-//         let vid = URL.createObjectURL(videoBlob);
-//         video.src = vid;
-//         video.play();
-//     }
-// };
-//
-// req.onerror = function() {
-//     console.log("wah wah");
-// };
-//
-// req.send();
